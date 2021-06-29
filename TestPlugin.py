3e9058aaf02aeaf0bf8c013b0e54d1fb6ece9a4f
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-06-29 11:47:35 -0400

    Update TestPlugin.py

--- TestPlugin.py
+++ TestPlugin.py
@@ -1,6 +1,6 @@
+import asyncio
 import os
 import tempfile
-# import time
 import unittest
 
 import nanome
@@ -19,47 +19,28 @@ CATEGORY = "testing"
 HAS_ADVANCED_OPTIONS = False
 
 
-class NanomeIntegrationTestLoader(unittest.TestLoader):
-    """A test loader which allows to parse keyword arguments to the test case class."""
-
-    def loadTestsFromTestCase(self, testCaseClass, plugin):
-        """Return a suite of all test cases contained in testCaseClass"""
-        if issubclass(testCaseClass, unittest.TestSuite):
-            raise TypeError("Test cases should not be derived from "
-                            "TestSuite. Maybe you meant to derive from "
-                            "TestCase?")
-        testCaseNames = self.getTestCaseNames(testCaseClass)
-        if not testCaseNames and hasattr(testCaseClass, 'runTest'):
-            testCaseNames = ['runTest']
-
-        loaded_suite = unittest.TestSuite()
-
-        testcase_instance = testCaseClass()
-        for test_name in testCaseNames:
-            loaded_suite.addTest(testcase_instance)
-        # loaded_suite = self.suiteClass(
-        #     map(testCaseClass, testCaseNames, [plugin for i in range(0, len(testCaseNames))]))
-        return loaded_suite
-
-
 class ChemInteractionsTestCase(unittest.TestCase):
     """Integration Tests for the Chemical Interactions Plugin."""
 
-    @property
-    def plugin(self):
-        return self._plugin
+    def __init__(self, testName, plugin=None):
+        super().__init__(testName)
+        self.plugin = plugin
 
-    @plugin.setter
-    def plugin(self, value):
-        self._plugin = value
+    def setUp(self):
+        self.loop = asyncio.get_event_loop()
+
+    def tearDown(self):
+        self.loop.create_task(self.clear_workspace())
 
     def test_tests_being_loaded(self):
         self.assertEqual(1, 1)
 
-    async def test_calculate_integrations_1tyl(self, plugin):
+    def test_calculate_interactions_1tyl(self):
         # Collect data to populate calculate_interactions method.
-        self.plugin = plugin
-        self.setup_1tyl_workspace()
+        self.loop.run_until_complete(self.calculate_interactions_1tyl(self.plugin))
+
+    async def calculate_interactions_1tyl(self, plugin):
+        await self.setup_1tyl_workspace()
         complex_list = await self.plugin.request_complex_list()
         comp = (await self.plugin.request_complexes([complex_list[0].index]))[0]
         ligand = None
@@ -84,7 +65,11 @@ class ChemInteractionsTestCase(unittest.TestCase):
         comp = Complex.io.from_pdb(path=filepath)
         workspace = Workspace()
         workspace.add_complex(comp)
-        self.update_workspace(workspace)
+        self.plugin.update_workspace(workspace)
+
+    async def clear_workspace(self):
+        workspace = Workspace()
+        self.plugin.update_workspace(workspace)
 
 
 class IntegrationTestPlugin(nanome.AsyncPluginInstance):
@@ -100,14 +85,13 @@ class IntegrationTestPlugin(nanome.AsyncPluginInstance):
         plugin_instance = self.plugin_class()
         plugin_instance._network = self._network
 
-        # loader = unittest.TestLoader()
-        loader = NanomeIntegrationTestLoader()
-        suite = loader.loadTestsFromTestCase(ChemInteractionsTestCase, plugin_instance)
+        suite = unittest.TestSuite()
+        suite.addTest(ChemInteractionsTestCase('test_calculate_interactions_1tyl', plugin_instance))
+        unittest.TextTestRunner(verbosity=2).run(suite)
         # add tests to the test suite
         # suite.addTest(ChemInteractionsTestCase(plugin_instance))
-        runner = unittest.TextTestRunner(verbosity=3)
-        runner.run(suite)
-        print('DOne!')
+        # runner = unittest.TextTestRunner(verbosity=3)
+        # runner.run(suite)
 
 
 nanome.Plugin.setup(NAME, DESCRIPTION, CATEGORY, False, IntegrationTestPlugin)
